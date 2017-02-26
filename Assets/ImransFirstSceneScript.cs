10596fe620f71b3dcba0b9ac34d5fd7f17fea976
ipeerbhai <ipeerbhai@aol.com>
2017-02-25 20:34:38 -0800

    Hello world now works!  No ability to read the controller...

--- Assets/ImransFirstSceneScript.cs
+++ Assets/ImransFirstSceneScript.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ImransFirstSceneScript : MonoBehaviour {
-
+    static private int m_callCount = 0; // how often has this class been called?
 	// Use this for initialization
 	void Start () {
-		
+    }
+
+    // Update is called once per frame
+    void Update () {
+        var MySphere = GameObject.Find("Sphere");
+        var MySphereTransform = MySphere.transform;
+        //MySphereTransform.Translate()
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per physics engine call, usually before update
+    private void FixedUpdate()
+    {
+        // update all the forces we want to...
+        var mySphere = GameObject.Find("Sphere");
+        var theRigidBody = mySphere.GetComponent<Rigidbody>();
+        if (mySphere.transform.position.y < 0.51)
+            theRigidBody.AddForce(0, 300, 0, ForceMode.Acceleration);
+    }
 }
