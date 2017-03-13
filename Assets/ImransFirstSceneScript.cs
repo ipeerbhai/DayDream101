b5b9fdafff1687766097c06f073412968fe8b3f1
Imran Peerbhai <ipeerbhai@aol.com>
2017-03-12 18:33:17 -0700

    Working run and kick ball in Unity PC player.

--- Assets/ImransFirstSceneScript.cs
+++ Assets/ImransFirstSceneScript.cs
@@ -2,31 +2,67 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ImransFirstSceneScript : MonoBehaviour {
-    private const float m_forwardSpeed = 3.0f;
-	// Use this for initialization
-	void Start () {
+public class ImransFirstSceneScript : MonoBehaviour
+{
+    private float m_forwardSpeed = 3.0f;
+    private float m_rotationSpeed = 10.0f;
+
+    private string m_RecordingDevice;
+
+    // Use this for initialization
+    void Start()
+    {
+        // let's figure out how to record sound in Unity.
+        foreach (string micDevice in Microphone.devices)
+        {
+            m_RecordingDevice = micDevice;
+        }
+
+        //Microphone.Start(m_RecordingDevice, false, 15, 16000);
+
     }
 
     // Update is called once per frame
-    void Update ()
+    void Update()
     {
+        float deadZone = 0.15f;
+
+        // put some debug output into the world.
+        var theTextGameObject = GameObject.Find("txtMainData");
+        UnityEngine.UI.Text theTextComponent = theTextGameObject.GetComponent<UnityEngine.UI.Text>();
+        var player = GameObject.Find("Player");
+        var camera = GameObject.Find("Main Camera");
+
+        // let's rotate the camera.
+        if (GvrController.IsTouching)
+        {
+            if (GvrController.TouchPos.x < .5 - deadZone)
+            {
+                // Should be rotating left
+                player.transform.Rotate(0, -1 * Time.deltaTime * m_rotationSpeed, 0);
+            }
+            else if (GvrController.TouchPos.x > .5 + deadZone)
+            {
+                //Should be rotating right
+                player.transform.Rotate(0, 1 * Time.deltaTime * m_rotationSpeed, 0);
+            }
+        }
+
         // let's move the camera when the user pushes the controller "click" button.
         if (GvrController.ClickButton)
         {
-            var theTextGameObject = GameObject.Find("txtMainData");
-            UnityEngine.UI.Text theTextComponent = theTextGameObject.GetComponent<UnityEngine.UI.Text>();
-            theTextComponent.text = "You have clicked!";
-            var camera = GameObject.Find("Main Camera");
-            camera.transform.position += (Vector3.forward * Time.deltaTime) * m_forwardSpeed; // using deltatime means I'm moving at 1 meter/s
+            // let's translate the camera forward
+            player.transform.position += (player.transform.rotation * Vector3.forward * Time.deltaTime) * m_forwardSpeed; // using deltatime means I'm moving at 1 meter/s
         }
     }
 
-    // Update is called once per physics engine call, usually before update
+
+    // FixedUpdate is called once per physics engine call, usually before update
     private void FixedUpdate()
     {
         // find the bouncing sphere from inside this central game object.
         var mySphere = GameObject.Find("Sphere"); // Can skip getting if this script component is attached to the GameObject that will be the target.
+        var sphereRigidBody = mySphere.GetComponent<Rigidbody>();
         var MySphereTransform = mySphere.transform;
 
         // find the controller and get its position.
@@ -41,11 +77,15 @@ public class ImransFirstSceneScript : MonoBehaviour {
             var theTextGameObject = GameObject.Find("txtMainData");
             UnityEngine.UI.Text theTextComponent = theTextGameObject.GetComponent<UnityEngine.UI.Text>();
             theTextComponent.text = "hit " + pointingAtWhat.collider.name;
+            if (GvrController.AppButton)
+            {
+                Vector3 forceToAdd = GvrController.Orientation * Vector3.forward * 100;
+                sphereRigidBody.AddForceAtPosition(forceToAdd, pointingAtWhat.point);
+            }
         }
 
         // update all the forces we want to...
-        var theRigidBody = mySphere.GetComponent<Rigidbody>();
         if (mySphere.transform.position.y < 0.51)
-            theRigidBody.AddForce(0, 300, 0, ForceMode.Acceleration);
+            sphereRigidBody.AddForce(0, 300, 0, ForceMode.Acceleration);
     }
 }
